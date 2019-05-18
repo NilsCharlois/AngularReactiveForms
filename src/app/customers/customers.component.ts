af0a86d1b465c529b79663e373e9e91a9033e156
Nils Charlois <nils.charlois@gmail.com>
2019-05-18 17:30:40 +0100

    Added validators and on the fly validations

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 import { Customer } from "./Customer";
 
@@ -12,20 +12,40 @@ export class CustomersComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      emailAddress: new FormControl(),
-      sendCatalog: new FormControl(true)
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName:['', [Validators.required, Validators.maxLength(50)]],
+      emailAddress:['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
+      sendCatalog:true
+    });
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName:'Harkness',
+      sendCatalog: false
     });
   }
 
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if(notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 
 }
