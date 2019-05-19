96e45b1d2dee69c2605d225b247fdff7262b8e8b
nils_charlois <nils.charlois@gmail.com>
2019-05-19 16:48:11 +0100

    Added cross-field validation with email confirmation

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -3,6 +3,23 @@ import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from
 
 import { Customer } from "./Customer";
 
+function emailMatcher(c:AbstractControl) : { [ key : string ] : boolean} | null {
+  const emailControl = c.get('emailAddress')
+  const confirmControl = c.get('confirmEmailAddress')
+
+  // not yet been touched
+  if(emailControl.pristine || confirmControl.pristine){
+    return null;
+  }
+
+  if(emailControl.value === confirmControl.value) {
+    return null;
+  }
+  // return the issue
+  return {'match':true}
+}
+
+// check that a given number is between 2 given values
 function ratingRange(min:number, max:number):ValidatorFn {
   return (c:AbstractControl):{[key:string]:boolean}|null => {
     if(c.value !== null && (isNaN(c.value) || c.value < min ||c.value > max)) {
@@ -29,7 +46,10 @@ export class CustomersComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName:['', [Validators.required, Validators.maxLength(50)]],
-      emailAddress:['', [Validators.required, Validators.email]],
+      emailGroup: this.fb.group({
+        emailAddress:['', [Validators.required, Validators.email]],
+        confirmEmailAddress:['', [Validators.required]]
+      }, {validator: emailMatcher}),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
